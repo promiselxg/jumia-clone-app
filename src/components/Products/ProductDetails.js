3d9fc64d50b29e88edbe93787f7b2e2d030e8974
Promiselxg <okeydeede@gmail.com>
2021-02-04 16:28:00 +0100

    added product details page

--- src/components/Products/ProductDetails.js
+++ src/components/Products/ProductDetails.js
@@ -2,6 +2,11 @@ import React from "react";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import States from "./States";
+import LocalShippingIcon from "@material-ui/icons/LocalShipping";
+import TouchAppIcon from "@material-ui/icons/TouchApp";
+import AllInboxIcon from "@material-ui/icons/AllInbox";
+import ReplayIcon from "@material-ui/icons/Replay";
 
 const ProductDetails = () => {
   return (
@@ -50,6 +55,64 @@ const ProductDetails = () => {
             </Button>
           </div>
           <Divider className='divider' />
+        </div>
+      </div>
+      <div className='productDetails__right'>
+        <div className='productDetails__rightContainer'>
+          <h1>DELIVERY &amp; RETURNS</h1>
+          <Divider className='divider' />
+          <h1>Choose your location</h1>
+          <States />
+          <div className='productDetails__delivery'>
+            <div>
+              <span>
+                <LocalShippingIcon />
+              </span>
+              <span>
+                <h3>Door Delivery</h3>
+                <p>
+                  Ready for delivery between 25 Mar &amp; 1 Apr when you order
+                  within next 22hrs 14mins
+                </p>
+              </span>
+            </div>
+            <div>
+              <span>
+                <TouchAppIcon />
+              </span>
+              <span>
+                <h3>Pickup Station</h3>
+                <p>
+                  Ready for delivery between 25 Mar &amp; 1 Apr when you order
+                  within next 22hrs 14mins
+                </p>
+              </span>
+            </div>
+            <div>
+              <span>
+                <AllInboxIcon />
+              </span>
+              <span>
+                <h3>Postal Shipping</h3>
+                <p>
+                  Ready for delivery between 6 Apr &amp; 4 May when you order
+                  within next 22hrs 14mins
+                </p>
+              </span>
+            </div>
+            <div>
+              <span>
+                <ReplayIcon />
+              </span>
+              <span>
+                <h3>Return Policy</h3>
+                <p>
+                  Free return within 15 days for Jumia Mall items and 7 days for
+                  other eligible items.
+                </p>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Products/States.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const States = () => {
+  return (
+    <>
+      <form className='form-select'>
+        <select name='' id=''>
+          <option value=''>State</option>
+          <option value=''>Abuja</option>
+          <option value=''>Lagos</option>
+          <option value=''>Oyo</option>
+          <option value=''>Enugu</option>
+        </select>
+        <select name='' id=''>
+          <option value=''>Local Government</option>
+          <option value=''>Abuja</option>
+          <option value=''>Lagos</option>
+          <option value=''>Oyo</option>
+          <option value=''>Enugu</option>
+        </select>
+      </form>
+    </>
+  );
+};
+
+export default States;
